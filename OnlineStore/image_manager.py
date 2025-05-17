869d9305c84643764afaf77476478703536ad2f0
Sergei Senichkin <61311905+DefaultX-od@users.noreply.github.com>
2025-05-17 13:44:48 +0300

    Update image_manager.py

--- OnlineStore/image_manager.py
+++ OnlineStore/image_manager.py
@@ -10,14 +10,12 @@ client_secret = os.getenv('client_secret')
 client = ImgurClient(client_id, client_secret)
 
 def get_image(img_id):
-    # link = '/static/img/no-photos.png'
     if img_id is None:
         link = '/static/img/no-photos.png'
     else:
         link=f'https://i.imgur.com/{img_id}.png'
     return link
 
-# get album images as list
 def get_images(album_id):
     alist = []
     if album_id is None:
